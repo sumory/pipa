f3fb394792fdeb6f4d378426c3129cfa9530fd8e
sumory <sumory.wu@gmail.com>
2014-12-01 00:21:41 +0800

    add test sample implements and README

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# pipa
+## pipa
 
+pipe anything.
 
-## introduction
+```
+
+                              XXXXX X    XXXXX    X
+                              X   X X    X   X   X X
+                              XXXXX X    XXXXX  X X X
+                              X     X    X     X     X
+                              X     X    X     X     X
+              +-------------------------------------------------------+
+              |                                                       |
++--------+    |                                                       |    +--------+
+|        |    |                     +-----------+                     |    |        |
+|  file  |    |     +--------+      |           |      +--------+     |    |  file  |
+| string +--> |     | source +----> |  channel  +----> |  sink  |     +--> | string |
+|  etc.  |    |     +--------+      |           |      +--------+     |    |  etc.  |
+|        |    |                     +-----------+                     |    |        |
++--------+    |                                                       |    +--------+
+              |                                                       |
+              +-------------------------------------------------------+
+
+```
+
 
  - inspired by [flume](http://flume.apache.org/)
  - still under heavy development, use at your own risk
---
+++ pipa_test.go
@@ -0,0 +1,151 @@
+package pipa
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+// Source
+type SampleSource struct {
+	msg    string
+	size   int
+	status int
+	input  chan interface{}
+	output chan interface{}
+}
+
+func NewSampleSource(msg string, size int) (ds *SampleSource) {
+	ds = &SampleSource{}
+	ds.size = size
+	ds.msg = msg
+	ds.status = WAITING
+	return
+}
+
+func (self *SampleSource) Status() int {
+	return self.status
+}
+
+func (self *SampleSource) Run() {
+	go func() {
+		for i := 0; i < self.size; i++ {
+			self.output <- self.msg
+		}
+		close(self.output)
+	}()
+	self.status = RUNNING
+}
+
+func (self *SampleSource) ConnectSource() (output chan interface{}, err error) {
+	self.output = make(chan interface{})
+	self.status = READY
+	return self.output, nil
+}
+
+// Sink
+type SampleSink struct {
+	status int
+	items  int
+	stop   chan bool
+	input  chan interface{}
+	msg interface{}
+}
+
+func NewSampleSink() (ns *SampleSink) {
+	ns = &SampleSink{}
+	ns.status = WAITING
+	ns.items = 0
+	return
+}
+
+func (self *SampleSink) Status() int {
+	return self.status
+}
+
+func (self *SampleSink) Run() {
+	go func() {
+		for {
+			stuff := <-self.input
+			if stuff == nil {
+				break
+			}else{
+				self.msg = stuff
+			}
+			self.items++
+		}
+		self.stop <- true
+		close(self.stop)
+	}()
+	self.status = RUNNING
+}
+
+func (self *SampleSink) ConnectSink(input chan interface{}) (stop chan bool, err error) {
+	self.stop = make(chan bool)
+	self.input = input
+	self.status = READY
+	return self.stop, nil
+}
+
+// pipe
+type SamplePipe struct {
+
+	status int
+	output chan interface{}
+	input  chan interface{}
+}
+
+func NewSamplePipe() (p *SamplePipe) {
+	p = &SamplePipe{}
+	return
+}
+
+func (p *SamplePipe) Status() int {
+	return p.status
+}
+
+func (p *SamplePipe) Run() {
+	go func() {
+		for {
+			stuff, ok := <-p.input
+			if !ok {
+				break
+			}
+			stufs := stuff.(string)
+			_ = stufs
+			p.output <- stuff
+		}
+		close(p.output)
+	}()
+	p.status = RUNNING
+}
+
+func (p *SamplePipe) ConnectPipe(input chan interface{}) (output chan interface{}, err error) {
+	p.output = make(chan interface{})
+	p.input = input
+	p.status = READY
+	return p.output, nil
+}
+
+func TestPipa(t *testing.T) {
+	Convey("test pipa", t, func() {
+			content := "hello pipa!"
+			count := 100
+			pl := NewPipa()
+
+			source := NewSampleSource(content, count)
+			pl.AddSource(source)
+
+			sink := NewSampleSink()
+			pl.AddSink(sink)
+
+			pl.AddPipe(NewSamplePipe())
+			pl.AddPipe(NewSamplePipe())
+
+			stop := pl.Connect()//all 3 parts Connect*
+			pl.Run()// all 3 parts start
+			<-stop
+
+			So(sink.items, ShouldEqual, count)
+			So(sink.msg,ShouldEqual, content)
+		})
+}
